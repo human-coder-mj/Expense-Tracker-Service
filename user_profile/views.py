97c9921c485334bff0ed62e06447f39911620ffb
human-coder-mj <jhamanas4532@gmail.com>
2025-01-29 14:19:06 +0530

    created user view for retrieving the users

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser] # only admin can request the users
