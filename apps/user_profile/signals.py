a39ab252b06e1c0bb5e80bdfcc1118e75152771e
human-coder-mj <jhamanas4532@gmail.com>
2025-03-14 01:53:13 +0530

    Updated signals for deleting profile on user deletion

--- apps/user_profile/signals.py
+++ apps/user_profile/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
@@ -7,3 +7,12 @@ from .models import Profile
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance, firstName=instance.username)
+
+@receiver(pre_delete, sender=User)
+def delete_user_profile(sender, instance, **kwargs):
+    """
+    Deletes the Profile when the User is deleted.
+    """
+    profile = Profile.objects.filter(user=instance).first()
+    if profile:
+        profile.delete()
