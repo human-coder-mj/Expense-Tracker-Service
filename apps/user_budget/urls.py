580691b2d297973129ea274555b758424d7ddff5
human-coder-mj <jhamanas4532@gmail.com>
2025-03-23 00:52:53 +0530

    created the budgets CRUD route

---
+++ apps/user_budget/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from .views import BudgetViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'budget', BudgetViewSet, basename='budget')
+
+urlpatterns = [
+    path('api/', include((router.urls))),
+]
--- apps/user_budget/views.py
+++ apps/user_budget/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.exceptions import APIException, NotFound
+from django.db.models import Sum
+from django.shortcuts import get_object_or_404
+from .models import Budget
+from .serializers import BudgetSerializer
 
-# Create your views here.
+class BudgetViewSet(viewsets.ModelViewSet):
+
+    serializer_class = BudgetSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        
+        return Budget.objects.filter(user=self.request.user) # Ensures users can only access their own budgets.
+
+    def perform_create(self, serializer):
+        
+        serializer.save(user=self.request.user) # Automatically assigns the authenticated user to the budget when creating.
+
+    def update(self, request, pk=None):
+        try:
+            current_budget = Budget.objects.get(pk=pk, user=request.user)
+        except Budget.DoesNotExist:
+            raise NotFound(detail="Budget does not exist", code=status.HTTP_404_NOT_FOUND)
+        
+        data = request.data.copy()
+        data['user'] = request.user.pk
+
+        # Ensure the new amount is not less than existing expenses
+        total_expense_amount = current_budget.expenses_set.aggregate(Sum('amount'))['amount__sum'] or 0
+        if 'amount' in data and float(data['amount']) < total_expense_amount:
+            return Response("This amount is already used in some expense",
+                               code=status.HTTP_424_FAILED_DEPENDENCY)
+        
+        serializer = self.get_serializer(current_budget, data=data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save(user=self.request.user)
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+        return Response(serializer.errors, status=status.HTTP_417_EXPECTATION_FAILED)
+
+    def destroy(self, request, pk=None):
+
+        try:
+            budget = Budget.objects.get(pk=pk, user=request.user)
+        except Budget.DoesNotExist:
+            raise NotFound(detail="Budget does not exist", code=status.HTTP_404_NOT_FOUND)
+
+        if budget.expenses_set.count() == 0:
+            budget.delete()
+            return Response({'detail': 'Budget has been deleted successfully'},
+                            status=status.HTTP_202_ACCEPTED)
+
+        raise APIException("Budget consists multiple expenses. Remove all the associated expenses first.",
+                           code=status.HTTP_406_NOT_ACCEPTABLE)
