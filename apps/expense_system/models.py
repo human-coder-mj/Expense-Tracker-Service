8bcdeb14eb0a687e33cdd43bed26d6e512d470f0
human-coder-mj <jhamanas4532@gmail.com>
2025-03-23 16:44:26 +0530

    Created the category model

--- apps/expense_system/models.py
+++ apps/expense_system/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from user_budget.models import Budget
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    desc = models.CharField(max_length=250, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_deleted = models.BooleanField(default=False) #This allows recovery and prevents accidental data loss.
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['title', 'user'], name='unique_category_per_user')
+        ]
+
+    def __str__(self):
+        return f"{self.title} ({self.user.username})"
+
