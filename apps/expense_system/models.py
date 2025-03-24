cf13ec8b27bc1ebab75b2a3afd4e256d0bebf0bb
human-coder-mj <jhamanas4532@gmail.com>
2025-03-24 23:46:24 +0530

    created the Expense Model for expense_system

--- apps/expense_system/models.py
+++ apps/expense_system/models.py
@@ -17,3 +17,18 @@ class Category(models.Model):
     def __str__(self):
         return f"{self.title} ({self.user.username})"
 
+
+class Expenses(models.Model):
+    title = models.CharField(max_length=50)
+    desc = models.CharField(max_length=250, blank=True)
+    expense_amount = models.DecimalField(max_digits=10 ,decimal_places=2, blank=False, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, blank=True)
+    budget = models.ForeignKey(Budget, null=False, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-added_at',)
+
+    def __str__(self):
+        return self.title
