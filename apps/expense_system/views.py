1d98264cbca3e9a3700cb15a802d656ae89196d5
human-coder-mj <jhamanas4532@gmail.com>
2025-03-25 10:33:56 +0530

    created the category route for CRUD operations

--- apps/expense_system/views.py
+++ apps/expense_system/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from .serializers import CategorySerializer
+from .models import Category
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+
+        return Category.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+
+        serializer.save(user=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+
+        instance = self.get_object()
+        data = {**request.data, "user": request.user.pk}
+        serializer = self.get_serializer(instance, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_205_RESET_CONTENT)
+
+        return Response(serializer.errors, status=status.HTTP_424_FAILED_DEPENDENCY)
+
+    def destroy(self, request, *args, **kwargs):
+
+        instance = self.get_object()
+        instance.delete()
+        return Response({'message': 'Category has been deleted'}, status=status.HTTP_204_NO_CONTENT)
