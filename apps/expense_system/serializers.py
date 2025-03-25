1dc0f596d0fa4b968478c83fce4d38872539e1db
human-coder-mj <jhamanas4532@gmail.com>
2025-03-25 17:48:06 +0530

    removed the unnecessary validation rule

--- apps/expense_system/serializers.py
+++ apps/expense_system/serializers.py
@@ -10,10 +10,3 @@ class CategorySerializer(serializers.ModelSerializer):
             'user': {'write_only': True},
             'is_deleted': {'write_only': True}
         }
-
-    def validate_title(self, value):
-        """Ensure the category title is unique per user."""
-        user = self.context['request'].user
-        if Category.objects.filter(title=value, user=user).exists():
-            raise serializers.ValidationError("You already have a category with this title.")
-        return value
