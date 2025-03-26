21eea8ecff04af188d206134e5dbbed047d65ce9
human-coder-mj <jhamanas4532@gmail.com>
2025-03-26 23:10:20 +0530

    created the expense serializer

--- apps/expense_system/serializers.py
+++ apps/expense_system/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Expenses
+from user_budget.serializers import BudgetSerializer 
 
 class CategorySerializer(serializers.ModelSerializer):
 
@@ -10,3 +11,34 @@ class CategorySerializer(serializers.ModelSerializer):
             'user': {'write_only': True},
             'is_deleted': {'write_only': True}
         }
+
+
+class ExpenseSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Expenses
+        fields = '__all__'
+        extra_kwargs = {
+            'user' : {'write_only' : True}
+        }
+
+    def validate_expense_amount(self, value):
+        """Ensure expense amount is greater than zero."""
+        if value <= 0:
+            raise serializers.ValidationError("Expense amount must be greater than zero.")
+        return value
+
+    def validate_categories(self, value):
+        """Ensure that the categories belong to the user."""
+        user = self.context['request'].user
+        for category in value:
+            if category.user != user:
+                raise serializers.ValidationError(f"Category '{category.title}' does not belong to the user.")
+        return value
+
+    def validate_budget(self, value):
+        """Ensure that the budget exists for the user."""
+        user = self.context['request'].user
+        if value.user != user:
+            raise serializers.ValidationError("This budget does not belong to the user.")
+        return value
